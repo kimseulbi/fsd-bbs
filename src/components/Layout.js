d14cbd6f45b040b9ae0c5ff48d2d09eaf5bbb0eb
kimseulbi <ksb5166@gmail.com>
2018-11-22 15:33:54 +0900

    로그아웃기능

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 // 무언가를 감싸는 컴포넌트를 만들때 사용 
 import React, { Component } from 'react'
 import {UserConsumer} from '../contexts/UserContext'
-
+// onClick={logout} === onClick={()=> logout()}
 export default class Layout extends Component {
   render() {
     return (
@@ -9,7 +9,10 @@ export default class Layout extends Component {
         <div className="header">
             헤더
             <UserConsumer>
-                {({ username }) => <div>{username}</div>}
+                {({ username, logout}) =><React.Fragment>
+                        <div>{username}</div>
+                        <button onClick={logout}>로그아웃</button>
+                </React.Fragment> }
             </UserConsumer>
         </div>
         <h1 className="title">{this.props.title}</h1>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -4,21 +4,21 @@ import api from "../api";
 const { Provider, Consumer } = React.createContext();
 
 export default class UserProvider extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         id: null,
-         username: null
-      }
-    }
+  constructor(props) {
+    super(props);
 
-    async componentDidMount(){
-        if(localStorage.getItem('token')){
-            await this.refreshUser()
-        }
+    this.state = {
+      id: null,
+      username: null
+    };
+  }
+
+  async componentDidMount() {
+    if (localStorage.getItem("token")) {
+      await this.refreshUser();
     }
-    
+  }
+
   // 이벤트 객체를 가질 필요가 없습니다.
   // 로그인 기능만 가지고 있음
   async login(username, password) {
@@ -27,19 +27,32 @@ export default class UserProvider extends Component {
       password
     });
     localStorage.setItem("token", res.data.token);
-    await this.refreshUser()
+    await this.refreshUser();
     // TODO: 게시글 목록 보여주기
   }
-async refreshUser(){
+
+  logout(){
+    // 로컬 스토리지에서 토큰 제거 
+    localStorage.removeItem('token')
+    // 사용 정보 캐시 초기화
+    this.setState({
+        id:null,
+        username: null
+    })
+    // TODO: 로그인 폼 보여주기
+    
+  }
+  async refreshUser() {
     const res2 = await api.get("/me");
     this.setState({ id: res2.data.id, username: res2.data.username });
-}
+  }
   render() {
-      // 로그인 했을때 상태 정보 바뀌기
+    // 로그인 했을때 상태 정보 바뀌기
     const value = {
       username: this.state.username,
       id: this.state.id,
-      login: this.login.bind(this)
+      login: this.login.bind(this),
+      logout: this.logout.bind(this)
     };
     return <Provider value={value}>{this.props.children}</Provider>;
   }

e7d336255565e415dc9ab3adb0158399021556c6
kimseulbi <ksb5166@gmail.com>
2018-11-26 12:42:22 +0900

    BEM + SCSS 사용

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../api";
 import Layout from "./Layout";
+import classNames from "classnames";
+import './PostList.scss';
 
 export default class PostList extends Component {
   constructor(props) {
@@ -8,33 +10,46 @@ export default class PostList extends Component {
 
     this.state = {
       posts: [],
-      loading: false
+      loading: true
     };
   }
   // 비동기 일지라도 기다려주지 않음
   async componentDidMount() {
     const res = await api.get("/posts");
     this.setState({
-      posts: res.data
+      posts: res.data,
+      loading: false
     });
   }
 
   render() {
     const { posts } = this.state;
-    const { onPostDetailPage, onNewPostFormPage, onLoginFormPage} = this.props;
+    const { onPostDetailPage, onNewPostFormPage, onLoginFormPage } = this.props;
+    const tilteClass = classNames(
+      "PostList__title",
+      // 객체
+      {
+        "PostList__title--loading": this.state.loading
+      }
+    );
     return (
       <Layout title="게시물 목록" onLoginFormPage={onLoginFormPage}>
-        <button onClick={() => onNewPostFormPage()}>새 글 쓰기</button>
-        <h1>게시물 목록</h1>
-        <ul>
-          {posts.map(post => (
-            <li key={post.id} onClick={() => onPostDetailPage(post.id)}>
-              {post.title}
-            </li>
-          ))}
-        </ul>
+        <div className="PostList">
+          <button onClick={() => onNewPostFormPage()}>새 글 쓰기</button>
+          <h1 className={tilteClass}>게시물 목록</h1>
+          <ul className="PostList__list">
+            {posts.map(post => (
+              <li
+                className="PostList-item"
+                key={post.id}
+                onClick={() => onPostDetailPage(post.id)}
+              >
+                {post.title}
+              </li>
+            ))}
+          </ul>
+        </div>
       </Layout>
     );
   }
 }
-
